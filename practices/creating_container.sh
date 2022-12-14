52315797f680364ad929a962c8309e674b82825a
Kattson Bastos <kattsonbastos@gmail.com>
2022-12-14 22:42:08 +0000

    docs: project 1 first md

--- practices/creating_container.sh
+++ practices/creating_container.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 docker run -d -p 8080:8080 -v "$PWD/dag_directory:/opt/airflow/dags/" \
---entrypoint=/bin/bash --name airflow apache/airflow:2.4.0-python3.8 \
+--entrypoint=/bin/bash \
+--name airflow apache/airflow:2.4.0-python3.8 \
 -c '(airflow db init && \
-airflow users create --username air --password teste@123 --role Admin --email air@air.br); \
-airflow webserver & airflow scheduler \
+    airflow users create --username air --password teste@123 --firstname Air --lastname Flow --role Admin --email admin@example.org
+    ); \
+airflow webserver & \
+airflow scheduler \
 '
