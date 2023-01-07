ee18e07e7847c7156adec010de2d6f19d2a77300
Kattson Bastos <kattsonbastos@gmail.com>
2023-01-07 12:35:39 +0000

    fix: changing file name

--- practices/dag_directory/simple_dag.py
+++ practices/dag_directory/simple_dag.py
@@ -10,22 +10,19 @@ from datetime import datetime, timedelta
 def _print_hello():
     return 'Heey from our first DAG!!'
 
-# creating the dag. It will be passed later to each task as \
-## an argument to the 'dag' parameter
-dag = DAG(
+with DAG(
     dag_id = 'hello_world', # task_id
     description='Hello World DAG',
     schedule_interval=timedelta(minutes=1), # the interval in which the dag will be executed
     start_date=datetime(2023,1,1), # its a timestemp mainly used in the attempt t obackfill
     catchup=False # interval in which the DAG will try to execute again past paused runs
-)
+) as dag:
 
-# python operator for the above function
-hello_task = PythonOperator(
-    task_id='hello_task',
-    python_callable=_print_hello, # <-- here comes the 'hello' function we created
-    dag=dag # <-- that's were the dag object we created comes in
-)
+    # python operator for the above function
+    hello_task = PythonOperator(
+        task_id='hello_task',
+        python_callable=_print_hello # <-- here comes the 'hello' function we created
+    )
 
-# just calling our task
-hello_task
+    # just calling our task
+    hello_task
