b6dd1618e30a8f06196121af4f2f7c0390b50e9e
Kattson Bastos <kattsonbastos@gmail.com>
2023-01-07 12:22:19 +0000

    fix: fixing task name

--- practices/dag_directory/01_first_dag.py
+++ practices/dag_directory/01_first_dag.py
@@ -6,15 +6,14 @@ from airflow.operators.python import PythonOperator
 # general
 from datetime import datetime, timedelta
 
-
 # this function is basically the task
-def print_hello():
+def _print_hello():
     return 'Heey from our first DAG!!'
 
 # creating the dag. It will be passed later to each task as \
 ## an argument to the 'dag' parameter
 dag = DAG(
-    dag_id = 'hello_world',
+    dag_id = 'hello_world', # task_id
     description='Hello World DAG',
     schedule_interval=timedelta(minutes=1), # the interval in which the dag will be executed
     start_date=datetime(2023,1,1), # its a timestemp mainly used in the attempt t obackfill
@@ -22,11 +21,11 @@ dag = DAG(
 )
 
 # python operator for the above function
-hello_operator = PythonOperator(
+hello_task = PythonOperator(
     task_id='hello_task',
-    python_callable=print_hello, # <-- here comes the 'hello' function we created
+    python_callable=_print_hello, # <-- here comes the 'hello' function we created
     dag=dag # <-- that's were the dag object we created comes in
 )
 
 # just calling our task
-hello_operator
+hello_task
